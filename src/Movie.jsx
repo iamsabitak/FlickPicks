ef3aa346f58a8fd0ae9cd1b08d59f8766ffa7645
sabita <sabitakhadka091@gmail.com>
2024-01-04 20:56:18 +0545

    Create Movie component to display fetched movies

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,33 +1,34 @@
-import React from "react";
+import { useGlobalContext } from "./contex/MovieState";
 import "./Movie.css";
 
-const movies = [
-  { id: 1, title: "Inception", genre: "Sci-Fi" },
-  { id: 2, title: "The Shawshank Redemption", genre: "Drama" },
-  { id: 3, title: "The Dark Knight", genre: "Action" },
-  { id: 4, title: "Pulp Fiction", genre: "Crime" },
-  { id: 5, title: "Forrest Gump", genre: "Drama" },
-  { id: 6, title: "The Matrix", genre: "Sci-Fi" },
-  { id: 7, title: "Interstellar", genre: "Sci-Fi" },
-  { id: 8, title: "Fight Club", genre: "Drama" },
-  { id: 9, title: "The Godfather", genre: "Crime" },
-  { id: 10, title: "Goodfellas", genre: "Crime" },
-  // Add more movies here
-];
-
-const Movie = () => {
+function Movie() {
+  const { movie, isloading } = useGlobalContext();
+  if (isloading) {
+    return (
+      <div className="loader">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
   return (
-    <div className="movie-list">
-      <h2>Movie List</h2>
-      <ul>
-        {movies.map(movie => (
-          <li key={movie.id} className="movie-item">
-            {movie.title} - {movie.genre}
-          </li>
-        ))}
-      </ul>
+    <div className="movie-container">
+      {movie.map((curMovie) => {
+        const { imdbID, Poster, Title, Year, Type } = curMovie;
+        const movieTitle = Title.substring(0, 16);
+
+        return (
+          <div className="movie-list" key={imdbID}>
+            <h3>
+              {movieTitle.length > 15 ? `${movieTitle}...` : movieTitle} {Type},{" "}
+              {Year}
+            </h3>
+            <img src={Poster} alt="Movie Poster" />
+            <div className="Add-to-Card">Add to Card</div>
+          </div>
+        );
+      })}
     </div>
   );
-};
+}
 
 export default Movie;

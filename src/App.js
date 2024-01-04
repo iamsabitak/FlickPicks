6638cad2d24341eeb09a11289db6db3d49ab3f78
sabita <sabitakhadka091@gmail.com>
2024-01-04 20:58:56 +0545

    Integrate MovieState context provider in App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import React, {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import Home from "./Home";
 
-import NavBar from "./Navbar";
-import HomePage from "./HomePage";
-import FavoritesPage from "./FavoritesPage";
-import FlickPicks from "./FlickPicks";
-import Movie from "./Movie";
-
-import "./App.css";
+import MovieState from "./contex/MovieState";
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route exact path="/home" element={<HomePage />} />
-        <Route path="/movies" element={<Movie />} />
-        <Route exact path="/" element={<FlickPicks />} />
-        <Route exact path="/favoritespage" element={<FavoritesPage />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    </Router>
+    <MovieState>
+      <Home />
+    </MovieState>
   );
 }
 

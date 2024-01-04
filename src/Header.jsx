a6da8f19ba3a0c151b267b4b0f1fdaf7b6d5f599
sabita <sabitakhadka091@gmail.com>
2024-01-04 20:38:21 +0545

    Add Header component for FlickPicks with title and description

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+function Header() {
+  return (
+    <div className="flickpicks-container">
+      <h1>FlickPicks</h1>
+      <h3>Discover and explore your favorite movies.</h3>
+    </div>
+  );
+}
+
+export default Header;

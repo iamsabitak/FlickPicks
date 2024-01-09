37b6d060463f0bfecc367e781f1a4735df899bab
sabita <sabitakhadka091@gmail.com>
2024-01-09 16:21:14 +0545

    Integrated routing with React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import Home from "./Home";
-
+import Favourite from "./Favourite";
 import MovieState from "./contex/MovieState";
+
+import NavBar from "./Navbar";
+
+import React, {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Movie from "./Movie";
 
 function App() {
   return (
     <MovieState>
-      <Home />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/movies" element={<Movie />} />
+          <Route exact path="/favourite" element={<Favourite />} />
+        </Routes>
+      </Router>
     </MovieState>
   );
 }

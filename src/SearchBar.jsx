e6e0ef423b89ea97abdba6d562b960bc0b798644
sabita <sabitakhadka091@gmail.com>
2024-01-09 16:19:57 +0545

    Updated SearchBar component in Home page

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 import "./Home.css";
 
 import { useGlobalContext } from "./contex/MovieState";
@@ -7,29 +8,31 @@ import { useGlobalContext } from "./contex/MovieState";
 function SearchBar() {
   const { query, setQuery, error } = useGlobalContext();
   return (
-    <div className="searchlocation">
-      <form
-        action="#"
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-        className="search-bar"
-      >
+    <>
+      <div className="searchlocation">
+        <form
+          action="#"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          className="search-bar"
+        >
+          <div>
+            <input
+              type="text"
+              placeholder="Search movies..."
+              className="search-input"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+          </div>
+        </form>
         <div>
-          <input
-            type="text"
-            placeholder="Search movies..."
-            className="search-input"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
+          <p>{error.show && error.msg}</p>
         </div>
-        {/* <button className="search-button">View Full Site</button> */}
-      </form>
-      <div>
-        <p>{error.show && error.msg}</p>
       </div>
-    </div>
+
+    </>
   );
 }
 
